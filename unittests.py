9d4f3538e926929f99b6505e2a17f9b75b891f61
Laura Lahesoo <laura.lahesoo@tum.de>
2018-11-05 23:41:47 +0100

    Add sample unit tests

--- unittests.py
+++ unittests.py
@@ -1,3 +1,4 @@
-def test_hello():
-    print("Hello Travis")
-    assert 1 == 1
+class Sample_unit_tests(unittest.TestCase):
+    def test_hello():
+        print("Hello Travis")
+        assert 1 == 1
